4c5a6d8f37ff94084238283807d2c168e0a16119
LSGhartley <98523493+LSGhartley@users.noreply.github.com>
2023-09-27 15:31:44 +0200

    added cloud database

--- app.py
+++ app.py
@@ -1,29 +1,19 @@
 from flask import Flask
 from flask import render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [{'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Midrand, South Africa',
-        'salary': 'FREE'}, {'id': 2,
-        'title': 'Front-End Developer',
-        'location': 'Midrand, South Africa',
-        'salary': 'FREE'}, {'id': 3,
-        'title': 'Backend Developer',
-        'location': 'Midrand, South Africa',
-        'salary': 'FREE'}, {'id': 4,
-        'title': 'Site Reliabilty Engineer',
-        'location': 'Midrand, South Africa',
-        'salary': 'FREE'}]
 
 @app.route("/")
 def homepage():
-    return render_template('home.html', jobs=JOBS, name="HEL")
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, name="HEL")
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
